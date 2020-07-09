eaf8e296ee2c224a0be6e40109a777c9a13ede2d
Kevin Yeh <kevinyeh@alum.mit.edu>
2020-07-09 00:04:05 -0400

    Updates

---
+++ server.ts
@@ -0,0 +1,45 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import * as fs from 'fs';
+import * as contentLoader from "./tools/contentLoader";
+
+
+let sourceInfo : contentLoader.sources = {
+	scripts : [
+				"https://cdnjs.cloudflare.com/ajax/libs/knockout/3.5.1/knockout-latest.min.js"
+				,"https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"
+				,"https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.bundle.min.js"
+				]
+	,templates :  [{original:"",modificationScript:""}]
+	,body : ""
+	,postProcess : []
+};
+
+const app = express();
+
+const myLoader = new contentLoader.contentLoader(sourceInfo);
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+async function serveTemplate(req: express.Request, res : express.Response)
+{
+	let templateName = req.params['template'];
+	
+}
+app.all("/templates/:template",serveTemplate);
+
+function mainContent(req: express.Request,res : express.Response)
+{
+	res.set({'Content-Type': 'text/html'});
+	res.write("<body>");
+	myLoader.generateStaticHTML(res);
+	res.write("Hello world!");
+	res.write("</body>");
+
+	res.status(200).send();
+}
+app.all("/main/",mainContent);
+const server = app.listen(8080,()=> { console.log("Listening on 8080");});
--- tools/contentLoader.ts
+++ tools/contentLoader.ts
@@ -1,23 +1,39 @@
 import express from "express";
- 
 function includeScript(response : express.Response, scriptURL : string)
 {
-	response.write("<script type='text/javascript' src='"+scriptURL+"'></script>"); 
+	response.write("<script type='text/javascript' src='"+scriptURL+"'></script>\n"); 
 }
-interface sources
+
+export interface templateDefinition
+{
+	original: string;
+	modificationScript : string;
+}
+
+export interface sources
 {
 	scripts : Array<string>;
-	templates : Array<string>;
+	templates :  Array<templateDefinition> ;
+	body : string;
+	postProcess : Array<string>;
 }
 
 export class contentLoader
 {
 	protected sourceInfo : sources;
+	protected templateList : Array<string>;
 	constructor (sourceInfo : sources)
 	{
 		this.sourceInfo = sourceInfo;
+		this.templateList = new Array<string>;
 	}
 	
+	protected processTemplate(template : templateDefinition)
+	{
+		import mod from template.modificationScript;
+		mod.process(template.original);
+		
+	}
 	public generateStaticHTML(response : express.Response)
 	{
 		for(let scriptIdx=0;scriptIdx<this.sourceInfo.scripts.length;scriptIdx++)
@@ -26,8 +42,14 @@ export class contentLoader
 		}
 		for(let templatesIdx=0;templatesIdx<this.sourceInfo.templates.length;templatesIdx++)
 		{
-			// retrieve templates and append to DOM
+			// Modifiy the templates as needed
+			this.processTemplate(this.sourceInfo.templates[templatesIdx]);
 			
 		}
+	}
+	
+	public getTemplateList()
+	{
+		return this.templateList;
 	}
 }
